f72f84ee8ed0ee8c1a9c58d6739d47b76ff0f8c3
Emate <richardemate@gmail.com>
2022-06-29 15:25:06 +0200

    change api base url

--- src/api/adverts247Api.js
+++ src/api/adverts247Api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// https://frozen-escarpment-07199.herokuapp.com/api
+// https://thawing-thicket-82190.herokuapp.com/api
+
+
 let instance = axios.create({
-    baseURL: `https://frozen-escarpment-07199.herokuapp.com/api`
+    baseURL: `https://thawing-thicket-82190.herokuapp.com/api`
 })
 
 
